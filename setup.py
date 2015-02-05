fb4919684e245fcd8cba88140dcb5be07c39687a
Sam Rushing <gitsam@rushing.nightmare.com>
2015-02-05 13:38:16 -0800

    probe for cython, if so compile _codec.pyx.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 # -*- Mode: Python -*-
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+
+try:
+    from Cython.Build import cythonize
+    exts = cythonize ([Extension ("tinyber._codec", ['tinyber/_codec.pyx'])])
+except ImportError:
+    exts = []
 
 setup (
     name             = 'tinyber_gen',
-    packages         = ['tinyber'],
+    packages         = find_packages(),
     description      = 'code generator for tinyber',
     scripts          = ['scripts/tinyber_gen'],
     package_data     = {'tinyber': ['data/*.[ch]', 'tinyber/codec.py']},
+    ext_modules      = exts
     )

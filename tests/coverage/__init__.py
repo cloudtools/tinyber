f39c78318e93c8b99b9c1d8c3a40a5320766a8b2
Sam Rushing <gitsam@rushing.nightmare.com>
2015-07-24 18:11:44 -0700

    generate t0_ber.py here, too.

--- tests/coverage/__init__.py
+++ tests/coverage/__init__.py
@@ -1,12 +1,11 @@
 
 # build t0_wrap.pyx before doing any of the other tests.
 
-from tests.utils import generate_c
+from tests.utils import generate_c, generate_py, test_reload
 
 print 'building from __init__ script...'
 generate_c ('tests/coverage/t0.asn', 't0', 'tests/coverage')
 from distutils.core import run_setup
 run_setup ('tests/coverage/setup.py', ['build_ext', '--inplace'])
 
-import t0_test_encoder
-import t0_c_driver
+generate_py ('tests/coverage/t0.asn', 't0', 'tests/coverage')

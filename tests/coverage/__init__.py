c7914ab40502fd1037f84b33e1abef0c18bf5d42
Sam Rushing <gitsam@rushing.nightmare.com>
2015-07-24 17:16:56 -0700

    more insane attempts.

--- tests/coverage/__init__.py
+++ tests/coverage/__init__.py
@@ -1,4 +1,5 @@
-#import t0_python_test
-import t0_gen_cases
+# This is my attempt to get test_build to run first.  It doesn't work.
+import t0_test_build
 import t0_test_encoder
+import t0_c_driver
 
